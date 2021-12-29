06c3110736a69aa876b0f97c4a83da7f772e4085
coleleg <coleleg@gmail.com>
2021-12-29 17:10:25 -0500

    created Manager object

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -8,3 +8,9 @@ test('creates a manager object', () => {
     expect(manager.email).toBe("manager@company.com");
     expect(manager.officeNumber).toBe("123");
 });
+
+test(`returns 'Manager'`, () => {
+    const manager = new Manager("John Doe", "12345", "manager@company.com", "123");
+
+    expect(manager.getRole()).toBe("Manager"); 
+});
